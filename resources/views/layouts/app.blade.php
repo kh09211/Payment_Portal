d56a82e84941d2929ded1b5c62c591ac7f96a869
Kyle <kh09211@georgiasouthern.edu>
2020-07-27 20:33:27 -0500

    fixed favicon problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <link rel="icon" type="image/png" href="favicon.ico">
+    
     <!-- Scripts -->
     <script src="https://js.stripe.com/v3/" type="text/javascript"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
